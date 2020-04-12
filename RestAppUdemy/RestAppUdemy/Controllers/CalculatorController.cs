33cfc141a14384654dde594f8696fcd9b2765d68
netogan <netogan@gmail.com>
2020-04-12 02:42:12 -0300

    Alterando metodos calculator

---
+++ RestAppUdemy/RestAppUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAppUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+                return decimalValue;
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double numericNumber;
+
+            bool isNumber = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out numericNumber);
+
+            return isNumber;
+        }
+    }
+}
