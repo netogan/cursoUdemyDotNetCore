1ff8fb47fb9006571b1d5114c4060b71f711f2ca
netogan <netogan@gmail.com>
2020-04-12 03:31:33 -0300

    Alteração da api usando interface

---
+++ RestAppUdemy/RestAppUdemy/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestAppUdemy.Model;
+
+namespace RestAppUdemy.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for(int i = 0; i < 8; i++)
+            {
+                Person p = MockPerson(i);
+                persons.Add(p);
+            }
+
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person()
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person LastName " + i,
+                Address = "Some Address " + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person()
+            {
+                Id = 1,
+                FirstName = "João",
+                LastName = "Da Silva",
+                Address = "Av. Sergipe",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
--- RestAppUdemy/RestAppUdemy/Startup.cs
+++ RestAppUdemy/RestAppUdemy/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestAppUdemy.Services;
+using RestAppUdemy.Services.Implementations;
 
 namespace RestAppUdemy
 {
@@ -26,6 +28,8 @@ namespace RestAppUdemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
